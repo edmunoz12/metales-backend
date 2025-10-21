eb17176c7aaa556786ec1ba9b8568edf2959f896
Edgar Muñoz <edmunoz@parpro.com>
2025-10-21 15:49:20 -0700

    configuracion de endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Principal\PrincipalController;
+use App\Http\Controllers\Tools\ToolsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::middleware('api')->group(function(){
+
+    Route::get('/menu',[PrincipalController::class,'index'])->name('menu');
+    Route::get('/submenu',[PrincipalController::class,'submenu'])->name('submenu');
+    //Route::get('/inicio',[PrincipalController::class,'sidebar'])->name('inicio');
+
+
+    //Route::get('/index', [ToolsController::class, 'index'])->name('tool');
+    //Route::post('newTool', [ToolsController::class, 'newTool'])->name('new-Tool');
+    Route::get('/tooTypes', [ToolsController::class, 'toolTypes'])->name('toolTypes');
+    Route::get('/locations', [ToolsController::class, 'locations'])->name('locations');
+    Route::get('/suppliers', [ToolsController::class, 'suppliers'])->name('suppliers');
+
+    Route::prefix('tools')->group(function () {
+        Route::get('/', [ToolsController::class, 'index']);     // Listar
+        Route::get('/{id}', [ToolsController::class, 'show']);  // Mostrar una
+        Route::post('/', [ToolsController::class, 'store']);    // Crear
+        Route::put('/{id}', [ToolsController::class, 'update']); // Actualizar
+        Route::delete('/{id}', [ToolsController::class, 'destroy']); // Eliminar
+    });
+
+    //Catalagos
+
+
+
+    //Route::apiResource('Unidad',UnidadController::class);
+   //
+
+
 });

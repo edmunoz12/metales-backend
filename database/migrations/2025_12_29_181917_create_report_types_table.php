cbdec3e92d6dc545ea9fe49301ffd008a8dafac3
Edgar Muñoz <edmunoz@parpro.com>
2025-10-31 07:21:49 -0700

    new migration report_types for tools

--- database/migrations/2025_12_29_181917_create_report_types_table.php
+++ database/migrations/2025_12_29_181917_create_report_types_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('report_types', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->string('code')->nullable();
+            $table->string('description')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 

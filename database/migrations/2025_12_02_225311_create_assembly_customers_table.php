903e31bff7ebe66b13f83dfc56be59bd57013f52
Edgar Muñoz <edmunoz@parpro.com>
2025-12-03 10:37:01 -0800

    add new assembly_customers migration and add logos folder.

---
+++ database/migrations/2025_12_02_225311_create_assembly_customers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assembly_customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->integer('certifications')->nullable();
+            $table->string('logo_path')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assembly_customers');
+    }
+};
---
+++ database/migrations/2025_12_03_163451_add_customer_id_to_assemblies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('assemblies', function (Blueprint $table) {
+            $table->foreignId('assembly_customer_id')
+                ->nullable()
+                ->constrained('assembly_customers')
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('assemblies', function (Blueprint $table) {
+            $table->dropForeign(['assembly_customer_id']);
+            $table->dropColumn('assembly_customer_id');
+        });
+    }
+};

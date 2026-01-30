64eecc007fb6017f7e4c16a76c4b595d78bdaa66
Edgar Muñoz <edmunoz@parpro.com>
2026-01-30 11:32:51 -0800

    fix broadcasting, websocket

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,11 +36,11 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => env('PUSHER_HOST', '127.0.0.1'),
-                'port' => env('PUSHER_PORT', 6001),
-                'scheme' => env('PUSHER_SCHEME', 'http'),
+                'cluster' => 'mt1',
                 'useTLS' => false,
-                'encrypted' => false,
+                'host' => 'ws.parprohub',
+                'port' => 80,
+                'scheme' => 'http',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html

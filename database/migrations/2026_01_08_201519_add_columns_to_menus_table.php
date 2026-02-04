6f7ded3ee5a341ceec69deecae53afce2bd32da8
Edgar Muñoz <edmunoz@parpro.com>
2026-02-04 12:23:58 -0800

    Add columns for performance

---
+++ database/migrations/2026_01_08_201519_add_columns_to_menus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            $table->foreignId('user_type_id')->nullable()->constrained('user_types', 'id')->nullOnDelete();
+            $table->integer('instance')->nullable();
+            $table->string('module')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            $table->dropForeign(['user_type_id']);
+            $table->dropColumn(
+                'user_type_id',
+                'instance',
+                'module',
+            );
+        });
+    }
+};

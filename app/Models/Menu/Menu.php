5b3ee58aa4a0663fd6b925f853f160c7dafe5f7f
Edgar Muñoz <edmunoz@parpro.com>
2026-02-17 15:24:40 -0800

    update models, fields and relationships

--- app/Models/Menu/Menu.php
+++ app/Models/Menu/Menu.php
@@ -2,10 +2,27 @@
 
 namespace App\Models\Menu;
 
+use App\Models\Submenu\Submenu;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Menu extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'menus';
+    protected $fillable = [
+        'menu',
+        'icono',
+        'order',
+        'active',
+        'user_id'
+    ];
+
+    public function submenus()
+    {
+        return $this->hasMany(Submenu::class);
+    }
 }
--- app/Models/Submenu/Submenu.php
+++ app/Models/Submenu/Submenu.php
@@ -2,10 +2,27 @@
 
 namespace App\Models\Submenu;
 
+use App\Models\Menu\Menu;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Submenu extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'submenu',
+        'componente',
+        'menu_id',
+        'orden',
+        'icono',
+        'active'
+    ];
+
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
 }
